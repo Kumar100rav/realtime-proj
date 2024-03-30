658cdaf5e7a7c2b882eb2919acb2a0549fd566e9
Kumar100rav <99313257+Kumar100rav@users.noreply.github.com>
2024-03-30 20:56:23 +0530

    Update setup.py

--- Predicting-Student-Performance-Using-Machine-Learning-main/setup.py
+++ Predicting-Student-Performance-Using-Machine-Learning-main/setup.py
@@ -21,8 +21,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name = "mlproject",
     version = '0.0.1',
-    author = 'sanjay',
-    author_email= 'skprasad117@gmail.com',
+    author = 'Kumar Saurav',
+    author_email= 'krsaurv11033@gmail.com',
     packages= find_packages(),
     install_requires = get_requirements('requirements.txt')
 )
